8b6761c6f3c739a106b6c08094c43416405d6673
dsastremadueno03 <davidsastremadu@gmail.com>
2026-01-14 12:43:30 +0100

    Implementación fábrica de Civilian, Attacker, Police

--- pedestrian-evacuation-simulator-cellular-automaton/src/main/java/es/uma/lcc/caesium/pedestrian/evacuation/simulator/cellular/automaton/automata/pedestrian/PedestrianFactory.java
+++ pedestrian-evacuation-simulator-cellular-automaton/src/main/java/es/uma/lcc/caesium/pedestrian/evacuation/simulator/cellular/automaton/automata/pedestrian/PedestrianFactory.java
@@ -14,14 +14,37 @@ public class PedestrianFactory {
   public PedestrianFactory(CellularAutomaton automaton) {
     this.automaton = automaton;
   }
+  
+  // Generación de diferentes tipos de Pedestrian
+  
+  public Civilian getCivilian(int row, int column, PedestrianParameters parameters, Age ageGroup) {
+	  validateCoordinates(row, column);
+	  return new Civilian(row, column, parameters, automaton, ageGroup);
+  }
+  
+  public Attacker getAttacker(int row, int column, PedestrianParameters parameters, int attackRange, int aggressiveness) {
+	  validateCoordinates(row, column);
+	  return new Attacker(row, column, parameters, automaton, attackRange, aggressiveness);
+  }
+  
+  public Police getPolice(int row, int column, PedestrianParameters parameters, int attackRange) {
+	  validateCoordinates(row, column);
+	  return new Police(row, column, parameters, automaton, attackRange);
+  }
 
+  // --------------
+  
   public Pedestrian getInstance(int row, int column, PedestrianParameters parameters) {
-    assert row >= 0 && row < automaton.getRows() : "getInstance: invalid row";
-    assert column >= 0 && row < automaton.getColumns() : "getInstance: invalid column";
     return new Pedestrian(row, column, parameters, automaton);
   }
 
   public Pedestrian getInstance(Location location, PedestrianParameters parameters) {
     return getInstance(location.row(), location.column(), parameters);
   }
+  
+  private void validateCoordinates(int row, int column) {
+	  assert row >= 0 && row < automaton.getRows() : "getInstance: invalid row";
+	    assert column >= 0 && row < automaton.getColumns() : "getInstance: invalid column";
+  }
+  
 }
