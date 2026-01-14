baccbb223fffa12ea29c243ac8b123655fcb5053
dsastremadueno03 <davidsastremadu@gmail.com>
2026-01-14 11:43:46 +0100

    Clase Attacker implementada

---
+++ pedestrian-evacuation-simulator-cellular-automaton/src/main/java/es/uma/lcc/caesium/pedestrian/evacuation/simulator/cellular/automaton/automata/pedestrian/Attacker.java
@@ -0,0 +1,37 @@
+package es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.pedestrian;
+
+import java.awt.Color;
+
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.CellularAutomaton;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.gui.Canvas;
+
+public class Attacker extends Pedestrian {
+	private final int attackRange;
+	// aggressiveness = 0% - 100% 
+	private final int aggressiveness;
+	
+	public Attacker(int row, int column, PedestrianParameters parameters, CellularAutomaton automaton, int attackRange, int aggressiveness){
+		super(row, column, parameters, automaton);
+		this.attackRange = attackRange;
+		this.aggressiveness = aggressiveness;
+	}
+
+	public int getAttackRange() {
+		return attackRange;
+	}
+
+	public int getAggressiveness() {
+		return aggressiveness;
+	}
+	
+	@Override
+	public void paint(Canvas canvas, Color fillColor, Color outlineColor) {
+		super.paint(canvas, Color.RED, outlineColor);
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " [Attacker: Range=" + attackRange + " cells, Aggressiveness=" + aggressiveness + "%]";
+	}
+	
+}
