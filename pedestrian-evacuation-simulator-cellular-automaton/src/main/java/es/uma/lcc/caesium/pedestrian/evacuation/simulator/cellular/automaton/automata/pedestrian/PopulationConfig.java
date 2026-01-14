b52a19ab9b24330f81ef8aece7d1f6c10e810e2f
dsastremadueno03 <davidsastremadu@gmail.com>
2026-01-14 12:50:00 +0100

    Creación de record para administrar los parámetros de los Pedestrians

---
+++ pedestrian-evacuation-simulator-cellular-automaton/src/main/java/es/uma/lcc/caesium/pedestrian/evacuation/simulator/cellular/automaton/automata/pedestrian/PopulationConfig.java
@@ -0,0 +1,22 @@
+package es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.pedestrian;
+
+public record PopulationConfig(
+		int numCivilians,
+		int numAttackers,
+		int numPolice,
+		double probChild,
+		double probAdult,
+		double probElderly) {
+	
+	public PopulationConfig {
+		if(numCivilians < 0 || numAttackers < 0 || numPolice < 0) {
+			throw new IllegalArgumentException("El número de agentes no puede ser negativo.");
+		}
+		
+		double sum = probChild + probAdult + probElderly;
+		if(Math.abs(sum - 1) > 0.001) {
+			System.err.println("Aviso: Las probabilidades de edades no suman 1,0.");
+		}
+	}
+
+}
