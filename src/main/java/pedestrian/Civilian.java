0452a0c9f7b9aa972e4eba5b58ab042900f3aea2
dsastremadueno03 <davidsastremadu@gmail.com>
2026-01-15 11:40:52 +0100

    Clase Civilian implementada

---
+++ src/main/java/pedestrian/Civilian.java
@@ -0,0 +1,26 @@
+package pedestrian;
+
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.CellularAutomaton;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.pedestrian.Pedestrian;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.pedestrian.PedestrianParameters;
+
+public class Civilian extends Pedestrian {
+
+	private final Age ageGroup;
+		
+		public Civilian(int row, int column, PedestrianParameters parameters, CellularAutomaton automaton, Age ageGroup){
+			super(row, column, parameters, automaton);
+			this.ageGroup = ageGroup;
+		}
+		
+		public Age getAge() {
+			return ageGroup;
+		}
+		
+		@Override
+		public String toString() {
+			return super.toString() + "[Civilian: " + ageGroup + "]";
+		}
+		
+}
+
