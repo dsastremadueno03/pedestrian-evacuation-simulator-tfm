11a682718f42d2654c067d3f1ba81f085245f105
dsastremadueno03 <davidsastremadu@gmail.com>
2026-01-19 19:17:28 +0100

    Implementado mapa estático de visibilidad

--- src/main/java/automaton/SpecificCellularAutomaton.java
+++ src/main/java/automaton/SpecificCellularAutomaton.java
@@ -3,17 +3,28 @@ package automaton;
 import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.CellularAutomaton;
 import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.CellularAutomatonParameters;
 import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.pedestrian.Pedestrian;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.geometry._2d.Location;
 import pedestrian.MultiPedestrianFactory;
 
 public class SpecificCellularAutomaton extends CellularAutomaton {
 	
 	private final MultiPedestrianFactory myFactory;
 	
+	// Matrix variables for static map
+	private boolean[][][][] visibilityMatrix;
+	private boolean visibilityCalculated = false;
+	
 	public SpecificCellularAutomaton(CellularAutomatonParameters parameters) {
 		super(parameters);
 		this.myFactory = new MultiPedestrianFactory(this);
 	}
 	
+	/**
+	 * Adds a specific pedestrian
+	 * 
+	 * @param pedestrian pedestrian to be added
+	 * @return true if possible to add
+	 */
 	public boolean addPedestrian(Pedestrian pedestrian) {
 		int row = pedestrian.getRow();
 		int column = pedestrian.getColumn();
@@ -34,5 +45,99 @@ public class SpecificCellularAutomaton extends CellularAutomaton {
 	public MultiPedestrianFactory getPedestrianFactory() {
 		return myFactory;
 	}
-
+	
+	public boolean isVisible(Location loc1, Location loc2) {
+		if (!visibilityCalculated) {
+			return checkLineOfSightWithObstacles(loc1.row(), loc1.column(), loc2.row(), loc2.column());
+		}
+		
+		return visibilityMatrix[loc1.row()][loc1.column()][loc2.row()][loc2.column()];
+	}
+	
+	/**
+	   * Checks if in line of sight (static obstacles)
+	   * 
+	   * @param r0 initial row
+	   * @param c0 initial column
+	   * @param r1 final row
+	   * @param c1 final column
+	   * @return true if in line of sight
+	   */
+	private boolean checkLineOfSightWithObstacles(int r0, int c0, int r1, int c1) {
+		if(r0 == r1 && c0 == c1)
+			  return true; // Same cell, always visible
+		  
+		  // Bresenham Algorithm
+		  // Distance to cover
+		  int dr = Math.abs(r1 - r0);
+		  int dc = Math.abs(c1 - c0);
+		  // Direction
+		  int sr = 1;
+		  int sc = 1;
+		  if(r0 > r1)
+			  sr = -1;
+		  if(c0 > c1)
+			  sc = -1;
+		  // Error trajectory calculation
+		  int error = dr - dc;
+		  
+		  int currentRow = r0;
+		  int currentCol = c0;
+		  
+		  while(true) {
+			  if(currentRow == r1 && currentCol == c1)
+				  return true;
+			  if((currentRow != r0 || currentCol != c0) && (scenario.isBlocked(currentRow, currentCol)))
+				  return false;
+			  
+			  // Fix trajectory
+			  int e2 = 2 * error;
+			  if(e2 > -dc) {
+				  error -= dc;
+				  currentRow += sr;
+			  }
+			  
+			  if(e2 < dr) {
+				  error += dr;
+				  currentCol += sc;
+			  }
+		  }
+		  
+	  }
+	
+	/**
+	 * TO BE CALLED IN MAIN
+	 */
+	public void calculateVisibilityMap() {
+		int rows = getRows();
+		int cols = getColumns();
+		
+		System.out.println("Calculating static visibility map...");
+		
+		long start = System.currentTimeMillis();
+		
+		visibilityMatrix = new boolean[rows][cols][rows][cols];
+		
+		for(int r0 = 0; r0 < rows; r0++){
+			for(int c0 = 0; c0 < cols; c0++){
+				for(int r1 = 0; r1 < rows; r1++){
+					for(int c1 = 0; c1 < cols; c1++){
+						if(r1 == r0 && c1 == c0){
+							visibilityMatrix[r0][c0][r1][c1] = true;
+						} 
+						else{
+							visibilityMatrix[r0][c0][r1][c1] = checkLineOfSightWithObstacles(r0, c0, r1, c1);
+						}
+					}
+				}
+			}
+			System.out.println("Calculating map, please wait... " + (Math.round((r0/(rows * 1.0)) * 100)));
+		}
+		visibilityCalculated = true;
+		long end = System.currentTimeMillis();
+		System.out.println("Map calculated in " + (end-start) + "ms.");
+		
+	}
+	
 }
+
