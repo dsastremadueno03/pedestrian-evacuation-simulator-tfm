eb1b8d83d9a7568d80f936b81ddc48ea7f9bdd2e
dsastremadueno03 <davidsastremadu@gmail.com>
2026-01-19 15:53:18 +0100

    Implementada clase SpecificCellularAutomaton y su addPedestrian

---
+++ src/main/java/automaton/SpecificCellularAutomaton.java
@@ -0,0 +1,30 @@
+package automaton;
+
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.CellularAutomaton;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.CellularAutomatonParameters;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.pedestrian.Pedestrian;
+
+public class SpecificCellularAutomaton extends CellularAutomaton {
+	
+	public SpecificCellularAutomaton(CellularAutomatonParameters parameters) {
+		super(parameters);
+	}
+	
+	public boolean addPedestrian(Pedestrian pedestrian) {
+		int row = pedestrian.getRow();
+		int column = pedestrian.getColumn();
+		
+		if(row < 0 || row >= getRows() || column < 0 || column >= getColumns()) {
+			return false;
+		}
+		
+		if(isCellReachable(row, column)) {
+			occupied[row][column] = true;
+			inScenarioPedestrians.add(pedestrian);
+			return true;
+		}
+		
+		return false;
+	}
+
+}
