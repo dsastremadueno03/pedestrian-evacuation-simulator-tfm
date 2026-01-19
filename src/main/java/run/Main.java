9f49fb74aaa72f5b69ab623d115e6f50203fac72
dsastremadueno03 <davidsastremadu@gmail.com>
2026-01-19 16:15:03 +0100

    Implementado el Main

---
+++ src/main/java/run/Main.java
@@ -0,0 +1,88 @@
+package run;
+
+import static es.uma.lcc.caesium.statistics.Random.random;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import com.github.cliftonlabs.json_simple.Jsoner;
+
+import automaton.SpecificCellularAutomaton;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.CellularAutomatonParameters;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.Statistics;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.neighbourhood.MooreNeighbourhood;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.pedestrian.Pedestrian;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.pedestrian.PedestrianParameters;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.scenario.examples.RandomScenario;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.cellular.automaton.automata.scenario.examples.Supermarket;
+import pedestrian.PopulationConfig;
+import pedestrian.PopulationGenerator;
+
+class Main {
+  public static void main(String[] args) {
+    random.setSeed();
+
+    var scenario = random.bernoulli(0.75) ? RandomScenario.randomScenario() : Supermarket.supermarket();
+
+    var cellularAutomatonParameters =
+        new CellularAutomatonParameters.Builder()
+            .scenario(scenario) // use this scenario
+            .timeLimit(10 * 60) // 10 minutes is time limit for simulation
+            .neighbourhood(MooreNeighbourhood::of) // use Moore's Neighbourhood for automaton
+            .pedestrianReferenceVelocity(1.3) // fastest pedestrians walk at 1.3 m/s
+            .GUITimeFactor(8) // perform GUI animation x8 times faster than real time
+            .build();
+
+    var automaton = new SpecificCellularAutomaton(cellularAutomatonParameters);
+
+    // Place pedestrians
+    
+    PopulationConfig populationConfig = new PopulationConfig(
+    		20, // numCivilians
+    		1, //numAttackers
+    		1, // numPolice
+    		0.1, // probChild
+    		0.7, // probAdult
+    		0.2 // probElderly
+    		);
+    
+    var pedestrianParametersSupplier =
+        new PedestrianParameters.Builder()
+            .fieldAttractionBias(random.nextDouble(1.0, 10.0 ))
+            .crowdRepulsion(random.nextDouble(0.1, 0.5))
+            .velocityPercent(random.nextDouble(0.3, 1.0))
+            .build();
+
+    var generator = new PopulationGenerator(automaton.getPedestrianFactory(), automaton);
+    
+    List<Pedestrian> crowd = generator.generatePopulation(populationConfig, pedestrianParametersSupplier);
+    
+    int addedCount = 0;
+    for(Pedestrian p : crowd) {
+    	if(automaton.addPedestrian(p)) {
+    		addedCount++;
+    	} else {
+    		System.out.println("Aviso: No se ha podido colocar " + p);
+    	}
+    }
+    System.out.println("Simulación iniciada con " + addedCount + " personas.");
+    
+    // ------------------
+    
+    automaton.runGUI(); // automaton.run() to run without GUI
+    Statistics statistics = automaton.computeStatistics();
+    System.out.println(statistics);
+
+    // write trace to json file
+    var trace = automaton.getTrace();
+    String fileName = "data/traces/trace.json";
+    try (FileWriter fileWriter = new FileWriter(fileName)) {
+      fileWriter.write(Jsoner.prettyPrint(trace.toJson().toJson()));
+      fileWriter.flush();
+      System.out.printf("Trace written to file %s successfully.%n", fileName);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
